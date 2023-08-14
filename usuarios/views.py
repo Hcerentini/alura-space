38c1ad90390ed08728730d4742f9156c2d9fc9cf
Henrique <henriquecerentini@yahoo.com>
2023-08-14 13:55:09 -0300

    Commit Final do Curso

---
+++ usuarios/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForm, CadastroForm
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
+
+def login(request):
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            nome=form['nome_login'].value()
+            senha=form['senha'].value()
+        
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        if usuario is not None:
+            auth.login(request,usuario)
+            messages.success(request, f"{nome} Logado com successo")
+            return redirect('index')
+        else:
+            messages.error(request, "Login failed")
+            return redirect('login')
+        
+
+    return render(request, "usuarios/login.html", {"form": form})
+
+def cadastro(request):
+    form = CadastroForm()
+
+    if request.method == 'POST':
+        form = CadastroForm(request.POST)
+
+        if form.is_valid():
+            
+            nome=form["nome_cadastro"].value()
+            email=form["email"].value()
+            senha=form["senha_1"].value()
+            
+            if User.objects.filter(username=nome).exists():
+                messages.error(request,"Usuário já existe")
+                return redirect('cadastro')
+            
+            usuario = User.objects.create_user(
+                username = nome,
+                email = email,
+                password = senha,
+            )
+            usuario.save()
+            messages.success(request,"Usuário cadastrado com sucesso")
+            return redirect('login')
+    return render(request, "usuarios/cadastro.html", {"form": form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout Success")
+    return redirect('login')
